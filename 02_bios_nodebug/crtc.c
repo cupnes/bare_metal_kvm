5a3c897606209d3ed29aae1d2729dcad818aa450
Yuma Ohgami <yuma@ohgami.jp>
2018-06-03 13:24:16 +0900

    edit

--- 02_bios_nodebug/crtc.c
+++ 02_bios_nodebug/crtc.c
@@ -4,31 +4,11 @@
 
 void crtc_handle_io(struct kvm_run *run)
 {
-	unsigned int i;
-
-	switch (run->io.direction) {
-	case KVM_EXIT_IO_IN:
-		for (i = 0; i < run->io.count; i++) {
-			switch (run->io.size) {
-			case 1:
-				*(unsigned char *)((unsigned char *)run + run->io.data_offset) = 0;
-				break;
-			case 2:
-				*(unsigned short *)((unsigned char *)run + run->io.data_offset) = 0;
-				break;
-			case 4:
-				*(unsigned short *)((unsigned char *)run + run->io.data_offset) = 0;
-				break;
-			}
-			run->io.data_offset += run->io.size;
-		}
-		break;
-
-	case KVM_EXIT_IO_OUT:
+	if (run->io.direction == KVM_EXIT_IO_OUT) {
+		unsigned int i;
 		for (i = 0; i < run->io.count; i++) {
 			putchar(*(char *)((unsigned char *)run + run->io.data_offset));
 			run->io.data_offset += run->io.size;
 		}
-		break;
 	}
 }
