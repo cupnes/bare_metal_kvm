2276e3453b9e1dda7795188ddccbd4c5790eda09
Yuma Ohgami <yuma@ohgami.jp>
2018-09-08 07:11:04 +0900

    02_bios: ram: use posix_memalign.

--- 02_bios/ram.c
+++ 02_bios/ram.c
@@ -1,13 +1,15 @@
-#include <sys/mman.h>
+#include <stdlib.h>
 #include "util.h"
 
 void ram_install(int vmfd, unsigned long long base, size_t size)
 {
-	void *addr = mmap(0, size, PROT_EXEC | PROT_READ | PROT_WRITE,
-			  MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-	assert(addr != MAP_FAILED, "ram: mmap");
+	int r;
+	void *addr;
 
-	int r = kvm_set_user_memory_region(vmfd, base, size,
+	r = posix_memalign(&addr, 4096, size);
+	assert(r == 0, "ram: posix_memalign");
+
+	r = kvm_set_user_memory_region(vmfd, base, size,
 				       (unsigned long long)addr);
 	assert(r != -1, "ram: KVM_SET_USER_MEMORY_REGION");
 }
