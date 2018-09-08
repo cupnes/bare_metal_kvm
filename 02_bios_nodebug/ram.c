c930654aa626f4e40c2e3bcb526941de358f983a
Yuma Ohgami <yuma@ohgami.jp>
2018-09-08 09:43:54 +0900

    edit

--- 02_bios_nodebug/ram.c
+++ 02_bios_nodebug/ram.c
@@ -1,9 +1,9 @@
-#include <sys/mman.h>
+#include <stdlib.h>
 #include "util.h"
 
 void ram_install(int vmfd, unsigned long long base, size_t size)
 {
-	void *addr = mmap(0, size, PROT_EXEC | PROT_READ | PROT_WRITE,
-			  MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	void *addr;
+	posix_memalign(&addr, 4096, size);
 	kvm_set_user_memory_region(vmfd, base, size, (unsigned long long)addr);
 }
